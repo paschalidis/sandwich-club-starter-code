5215cb3c5005026c1cf46ac0f85e924124c533ba
Pavlos <msf7415012@teilar.gr>
2018-03-25 04:27:35 +0300

    feat: populate sandwich data

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,10 +17,20 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    TextView mOriginTvTextView;
+    TextView mAlsoKnownAsTextView;
+    TextView mIngredientsTextView;
+    TextView mDescriptionTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mOriginTvTextView = findViewById(R.id.origin_tv);
+        mAlsoKnownAsTextView = findViewById(R.id.also_known_tv);
+        mIngredientsTextView = findViewById(R.id.ingredients_tv);
+        mDescriptionTextView = findViewById(R.id.description_tv);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
@@ -43,7 +55,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +68,10 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        mOriginTvTextView.setText(sandwich.getPlaceOfOrigin());
+        mAlsoKnownAsTextView.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+        mIngredientsTextView.setText(TextUtils.join(", ", sandwich.getIngredients()));
+        mDescriptionTextView.setText(sandwich.getDescription());
     }
 }
